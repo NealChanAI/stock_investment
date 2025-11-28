0f2a67a5ee13b5535a42a6ef2e58fb3e446833aa
chenyongming <nealchan@buaa.edu.cn>
2025-11-28 18:28:15 +0800

    a

--- src/test/akshare_valuation_test.py
+++ src/test/akshare_valuation_test.py
@@ -3,7 +3,6 @@ from akshare.stock.stock_zh_comparison_em import stock_zh_valuation_comparison_e
 
 
 def main():
-    
     stock_zh_valuation_comparison_em_df = stock_zh_valuation_comparison_em(symbol="SH601888")
     print(stock_zh_valuation_comparison_em_df)
 
